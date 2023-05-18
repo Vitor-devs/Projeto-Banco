806ada60979cd19c073b88947a32df80c3bf2146
Vitor Soares Coelho <102315159+Vitor-devs@users.noreply.github.com>
2023-05-18 10:22:01 -0300

    Update 2- View.sql sql server

--- 2- View.sql
+++ 2- View.sql
@@ -1,4 +1,4 @@
-#retorna informações sobre os filmes, diretores e estúdios
+--retorna informações sobre os filmes, diretores e estúdios
 CREATE VIEW InformacoesFilmes AS
 SELECT F.Titulo, F.AnoLancamento, F.Genero, F.Duracao, D.NomeDiretor AS NomeDiretor, E.NomeEstudio AS NomeEstudio
 FROM Filme F
@@ -6,5 +6,3 @@ JOIN Diretor D ON F.IdDiretor = D.IdDiretor
 JOIN Estudio E ON F.IdEstudio = E.IdEstudio;
 
 SELECT * FROM InformacoesFilmes;
-
-
